a49019b7b7d22b5f0467d79209c0eee0308731fc
juanlu-millan <juanlumillanhidalgo@gmail.com>
2019-03-04 18:11:33 +0100

    Creo fichero .py

---
+++ ejercicio_json.py
@@ -0,0 +1,14 @@
+import json
+with open("champions.json") as fichero:
+    datos=json.load(fichero)
+
+print (type (datos))
+# Lista el nombre de todos los equipos de la Champions
+
+# Cuenta la cantidad de victorias,empates y derrotas del equipo de elijas
+
+# Pide un equipo y te dice la cantidad de goles que ha marcado
+
+# Introduce la fecha del partido,muestra los equipos y el resultado final
+
+# Top 5 de máximos goleadores
